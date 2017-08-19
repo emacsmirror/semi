af70ef0df032fd74a28f41bf1eeb2d207f96e9d9
Jonas Bernoulli <jonas@bernoul.li>
2017-08-19 17:50:00 +0200

    [PATCH] Add library semi.el whose name matches that of the package semi
    A package should always contain a library with the same name.  One
    benefit of that is that Melpa can then extract the package description
    from the library commentary.
    
    Remove "semi-pkg.el".  The only additional information it contains is a
    version string, which has not ever been bumped since it was first added
    in 2014.
    
    [Emacsmirror] This commit is being rebased onto upstream.

--- semi-pkg.el
+++
@@ -1,6 +0,0 @@
-(define-package "semi" "1.14.7"
-  "A library to provide MIME features."
-  '((emacs "24.5")
-    (apel "10.8")
-    (flim "1.14.9"))
-  :maintainers '(("Kazuhiro Ito" . "kzhr@d1.dion.ne.jp")))
