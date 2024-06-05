34d174fef2639c75dc34b8cd19743b339ff4845a
Kazuhiro Ito <kzhr@d1.dion.ne.jp>
2024-06-05 18:47:30 +0900

    Don't ignore ':' in URL path
    * semi-def.el (mime-browse-url-regexp): Use
    browse-url-button-regexp when available.  Add ':' to regexp for
    URL path.
    
    Fix https://github.com/wanderlust/semi/issues/28

--- semi-def.el
+++ semi-def.el
@@ -97,9 +97,12 @@
 ;;;
 
 (defcustom mime-browse-url-regexp
-  (concat "\\(https?\\|ftps?\\|file\\|gopher\\|news\\|nntps?\\|telnets?\\|wais\\|mailto\\):"
-	  "\\(//[-a-zA-Z0-9_.]+:[0-9]*\\)?"
-	  "[-a-zA-Z0-9_=?#$@~`%&*+|\\/.,]*[-a-zA-Z0-9_=#$@~`%&*+|\\/]")
+  (if (boundp 'browse-url-button-regexp)
+      ;; Available since Emacs 27
+      browse-url-button-regexp
+    (concat "\\(https?\\|ftps?\\|file\\|gopher\\|news\\|nntps?\\|telnets?\\|wais\\|mailto\\):"
+	    "\\(//[-a-zA-Z0-9_.]+:[0-9]*\\)?"
+	    "[-a-zA-Z0-9_=?#$:@~`%&*+|\\/.,]*[-a-zA-Z0-9_=#$:@~`%&*+|\\/]"))
   "*Regexp to match URL in text body."
   :group 'mime
   :type 'regexp)
