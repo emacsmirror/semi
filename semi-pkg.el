8344d7dc29d981dbd0ed0159433c7cbe6553c15b
Kazuhiro Ito <kzhr@d1.dion.ne.jp>
2025-05-06 14:03:07 +0900

    * semi-pkg.el: Add :maintainers property.

--- semi-pkg.el
+++ semi-pkg.el
@@ -2,4 +2,5 @@
   "A library to provide MIME features."
   '((emacs "24.5")
     (apel "10.8")
-    (flim "1.14.9")))
+    (flim "1.14.9"))
+  :maintainers '(("Kazuhiro Ito" . "kzhr@d1.dion.ne.jp")))
