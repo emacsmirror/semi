8fe5281722dcd159817e4879a0150d7628347aef
RadioNoiseE <rne.kou@icloud.com>
2025-11-26 23:32:40 +0800

    Update description for mime-image-max-*
    * mime-image.el (mime-image-max-width, mime-image-max-height): Update
    description.

--- mime-image.el
+++ mime-image.el
@@ -79,7 +79,7 @@ When `mime-image-normalize-xbm' is non-nil, original size is
 always used for xbm image."
     :group 'mime-view
     :type '(choice (const :tag "Use original size" nil)
-		   (float :tag "Ratio to frame width")
+		   (float :tag "Ratio to frame height")
 		   (integer :tag "Specify in pixel")))
 
 (defcustom mime-image-max-width nil
@@ -92,7 +92,7 @@ When `mime-image-normalize-xbm' is non-nil, original size is
 always used for xbm image."
   :group 'mime-view
   :type '(choice (const :tag "Use original size" nil)
-		 (float :tag "Ratio to frame height")
+		 (float :tag "Ratio to frame width")
 		 (integer :tag "Specify in pixel")))
 
 (defcustom mime-image-normalize-xbm t
